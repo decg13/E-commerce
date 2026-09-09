ae9a11c1214bc724c9f1a35d444cb63bfa57670a
decg13 <declandude1@gmail.com>
2026-09-09 14:33:45 -0600

    Completed E Commerce Library Project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function openMenu () {
-    document.body.classlist += "menu--open"
+function openMenu() {
+    document.body.classList += " menu--open"
 }
 
-function closeMenu () {
-    document.body.classlist.remove('menu--open')
+function closeMenu() {
+    document.body.classList.remove('menu--open')
 }
